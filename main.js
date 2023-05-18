d12c8e2db0bb238fad34d9605e3a1c1d93f35a54
lrizza <leonardorizza@hotmail.com>
2023-05-18 20:44:12 -0300

    tercera preentrega ahora funciona todo

--- main.js
+++ main.js
@@ -1,66 +1,64 @@
+const autenticar = (event) => {
+    event.preventDefault(); // Evitar el envío del formulario
 
-//-------------------------------------------------//
-
-/* const listaAnimalesJSON = JSON.stringify(listaAnimales)
-
-localStorage.setItem("porRescatar", listaAnimalesJSON)
-
-const listaAnimalesLocalStorage = localStorage.getItem("porRescatar")
-
-const listaAnimalesparseado = JSON.parse(listaAnimalesLocalStorage) */
-
-//-------------------------------------------------//
-
-const autenticar = () => {
     const usuarioInput = document.getElementById('usuario');
     const passwordInput = document.getElementById('password');
     const nombreUsuario = usuarioInput.value;
     const contrasena = passwordInput.value;
 
-    if (nombreUsuario === "Admin89") {
-        if (contrasena === "pass999") {
+    if (nombreUsuario === "admin" && contrasena === "321321") {
+        Swal.fire({
+            icon: 'success',
+            title: '¡Ingreso exitoso!',
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 1000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer);
+                toast.addEventListener('mouseleave', Swal.resumeTimer);
+            }
+        });
+        setTimeout(() => {
+            window.location.href = "../html/administrar.html";
+        }, 1000);
+    } else {
+        if (nombreUsuario !== "admin") {
             Swal.fire({
-                icon: 'success',
-                title: '¡Ingreso exitoso!',
-                toast: true,
-                position: 'bottom-end',
-                showConfirmButton: false,
-                timer: 2000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                    toast.addEventListener('mouseenter', Swal.stopTimer);
-                    toast.addEventListener('mouseleave', Swal.resumeTimer);
-                }
+                icon: "error",
+                title: "Usuario inexistente.",
             });
-            return true;
         } else {
             Swal.fire({
                 icon: "error",
                 title: "Contraseña incorrecta.",
             });
-            return false;
         }
-    } else {
-        Swal.fire({
-            icon: "error",
-            title: "Usuario inexistente.",
-        });
-        return false;
     }
 };
 
-const ingreso = document.getElementById('ingreso');
-ingreso.addEventListener('submit', (event) => {
-    event.preventDefault(); // Evita el envío del formulario k se recargue la pag
-    if (autenticar()) {
-        mostrarMenu();
-    }
-});
+const ingresoForm = document.getElementById('ingreso');
+if (ingresoForm) {
+    ingresoForm.addEventListener('submit', autenticar);
+}
 
-const rescatadosBtn = document.getElementById('rescatadosBtn');
-rescatadosBtn.addEventListener('click', () => {
-    console.log(listaAnimalesparseado);
-});
+const autenticarBtn = document.getElementById('autenticarBtn');
+const formularioIngreso = document.getElementById('formularioIngreso');
+
+if (autenticarBtn && formularioIngreso) {
+    autenticarBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        formularioIngreso.style.display = 'block';
+    });
+}
+
+const cerrarBtn = document.getElementById('cerrarBtn');
+if (cerrarBtn && formularioIngreso) {
+    cerrarBtn.addEventListener('click', () => {
+        formularioIngreso.style.display = 'none';
+    });
+}
 
 let listaRescatados = [];
 
@@ -78,6 +76,71 @@ class Mascota {
 let listaAnimales = [];
 
 let numeroMascota = 1;
+
+const mostrarMascotas = () => {
+    let mensaje = "\nLista de Mascotas:\n\n";
+    for (let i = 0; i < listaAnimales.length; i++) {
+        mensaje += `Nombre: ${listaAnimales[i].nombre}\n`;
+        mensaje += `Edad: ${listaAnimales[i].edad}\n`;
+        mensaje += `Especie: ${listaAnimales[i].especie}\n`;
+        mensaje += `Sexo: ${listaAnimales[i].sexo}\n`;
+        mensaje += `Color: ${listaAnimales[i].color}\n`;
+        mensaje += `Número: ${listaAnimales[i].numero}\n\n`;
+    }
+    alert(mensaje);
+};
+
+const mascotaRescatada = () => {
+    let numero = prompt("Ingrese el número de Mascota rescatada: ");
+    let finder = false;
+    for (let i = 0; i < listaAnimales.length; i++) {
+        if (listaAnimales[i].numero == numero) {
+            const mascotaRescatada = listaAnimales.splice(i, 1)[0];
+            listaRescatados.push(mascotaRescatada);
+            finder = true;
+            break;
+        }
+    }
+    if (!finder) {
+        alert("No se encontró ninguna Mascota con ese número.");
+    }
+    console.log(listaRescatados);
+};
+
+const modificarMascota = () => {
+    let numero = prompt("Ingrese el número de Mascota a modificar:");
+    let finder = false;
+    for (let i = 0; i < listaAnimales.length; i++) {
+        if (listaAnimales[i].numero == numero) {
+            let opcion = prompt(`Ingrese el número de la opción que desea modificar:\n 1-Nombre \n 2-Especie \n 3-Edad \n 4-Sexo \n 5-Color`);
+            switch (opcion) {
+                case '1':
+                    listaAnimales[i].nombre = prompt("Ingrese el nuevo nombre:");
+                    break;
+                case '2':
+                    listaAnimales[i].especie = prompt("Ingrese la nueva especie:");
+                    break;
+                case '3':
+                    listaAnimales[i].edad = prompt("Ingrese la nueva edad:");
+                    break;
+                case '4':
+                    listaAnimales[i].sexo = prompt("Ingrese el nuevo sexo:");
+                    break;
+                case '5':
+                    listaAnimales[i].color = prompt("Ingrese el nuevo color:");
+                    break;
+                default:
+                    alert("Opción inválida");
+                    break;
+            }
+            finder = true;
+            break;
+        }
+    }
+    if (!finder) {
+        alert("No se encontró ninguna Mascota con ese número.");
+    }
+};
 
 const agregarMascotaFromForm = () => {
     const nombreInput = document.getElementById('nombreMascota');
@@ -118,109 +181,80 @@ const agregarMascotaFromForm = () => {
             toast.addEventListener('mouseleave', Swal.resumeTimer);
         }
     });
+
+    const listaAnimalesJSON = JSON.stringify(listaAnimales)
+
+    localStorage.setItem("porRescatar", listaAnimalesJSON)
 };
 
-const mostrarFormularioAgregarMascota = () => {
-    const agregarMascotaForm = document.getElementById('agregarMascotaForm');
-    agregarMascotaForm.style.display = 'block';
-};
+function agregarMascotaMostrar() {
+    var form = document.getElementById('agregarMascotaForm');
+    form.style.display = 'block';
+}
 
-
-/* const agregarMascota = () => {
-    let nombre = prompt("Ingrese el nombre de la Mascota:");
-    let especie = prompt("Ingrese la especie de la Mascota:");
-    let edad = prompt("Ingrese la edad de la Mascota:");
-    let sexo = prompt("Ingrese el sexo de la Mascota: (M o H)");
-    let color = prompt("Ingrese el color:");
-    let numero = numeroMascota;
-    numeroMascota++; // suma un numero para la siguiente mascota.
-    let nuevaMascota = new Mascota(nombre, especie, edad, sexo, color, numero);
-    listaAnimales.push(nuevaMascota);
-};
- */
-const modificarMascota = () => {
-    let numero = prompt("Ingrese el numero de Mascota a modificar:");
-    let finder = false;
-    for (let i = 0; i < listaAnimales.length; i++) {
-        if (listaAnimales[i].numero == numero) {
-            let opcion = prompt(`Ingrese el número de la opción que desea modificar:\n 1-Nombre \n 2-Especie \n 3-Edad \n 4-Sexo \n 5-Color`);
-            switch (opcion) {
-                case '1':
-                    listaAnimales[i].nombre = prompt("Ingrese el nuevo nombre:");
-                    break;
-                case '2':
-                    listaAnimales[i].especie = prompt("Ingrese la nueva especie:");
-                    break;
-                case '3':
-                    listaAnimales[i].edad = prompt("Ingrese la nueva edad:");
-                    break;
-                case '4':
-                    listaAnimales[i].sexo = prompt("Ingrese el nuevo sexo:");
-                    break;
-                case '5':
-                    listaAnimales[i].color = prompt("Ingrese el nuevo color:");
-                    break;
-                default:
-                    alert("Opción inválida");
-                    break;
-            }
-            finder = true;
-            break;
-        }
-    }
-    if (!finder) {
-        alert("No se encontró ninguna Mascota con ese numero.");
-    }
-};
-
-
-const mascotaRescatada = () => {
-    let numero = prompt("Ingrese el numero de Mascota rescatada:");
-    let finder = false;
-    for (let i = 0; i < listaAnimales.length; i++) {
-        if (listaAnimales[i].numero == numero) {
-            const mascotaRescatada = listaAnimales.splice(i, 1)[0];
-            listaRescatados.push(mascotaRescatada);
-            finder = true;
-            break;
-        }
-    }
-    if (!finder) {
-        alert("No se encontró ninguna Mascota con ese numero.");
-    }
-    console.log(listaRescatados)
-};
-
-const mostrarMascotas = () => {
-    let mensaje = "\nLista de Mascotas:\n\n";
-    for (let i = 0; i < listaAnimales.length; i++) {
-        mensaje += `Nombre: ${listaAnimales[i].nombre}\n`;
-        mensaje += `Edad: ${listaAnimales[i].edad}\n`;
-        mensaje += `Especie: ${listaAnimales[i].especie}\n`;
-        mensaje += `Sexo: ${listaAnimales[i].sexo}\n`;
-        mensaje += `Color: ${listaAnimales[i].color}\n`;
-        mensaje += `Número: ${listaAnimales[i].numero}\n\n`;
-    }
-    alert(mensaje);
-};
-
-const mostrarMenu = () => {
-    const ingresoForm = document.getElementById('ingreso');
-    ingresoForm.innerHTML = `
-        <div class="menu">
-            <button class="menuBtn" onclick="mostrarFormularioAgregarMascota()">Agregar Mascota</button>
-            <button class="menuBtn" onclick="mascotaRescatada()">Fue rescatado</button>
-            <button class="menuBtn" onclick="modificarMascota()">Modificar Mascota</button>
-            <button class="menuBtn" onclick="mostrarMascotas()">lista de Mascotas</button>
-            <button class="menuBtn" onclick="cerrarSesion()">Salir</button>
-        </div>
-    `;
-};
 
 const cerrarSesion = () => {
     Swal.fire({
         icon: "success",
-        title: "Sesion cerrada.",
+        title: "Sesión cerrada.",
     });
-    location.reload();
+
+    setTimeout(() => {
+        window.location.href = "../index.html";
+    }, 1000);
 };
+
+const formularioContacta = document.getElementById('contacta');
+const nombreInput = document.getElementById('nombre');
+const emailInput = document.getElementById('email');
+const celularInput = document.getElementById('celular');
+const mensajeInput = document.getElementById('mensaje');
+const enviarBtn = document.getElementById('consulta');
+
+const obtenerConsultasLocalStorage = () => {
+    const consultasJSON = localStorage.getItem('consultas');
+
+    if (consultasJSON) {
+        return JSON.parse(consultasJSON);
+    } else {
+        return [];
+    }
+};
+
+const guardarConsultasLocalStorage = (consultas) => {
+    const consultasJSON = JSON.stringify(consultas);
+
+    localStorage.setItem('consultas', consultasJSON);
+};
+
+const enviarConsulta = (event) => {
+    event.preventDefault();
+
+    const consultas = obtenerConsultasLocalStorage();
+
+    const nuevoConsulta = {
+        nombre: nombreInput.value,
+        email: emailInput.value,
+        celular: celularInput.value,
+        mensaje: mensajeInput.value,
+    };
+
+    consultas.push(nuevoConsulta);
+
+    guardarConsultasLocalStorage(consultas);
+
+    nombreInput.value = '';
+    emailInput.value = '';
+    celularInput.value = '';
+    mensajeInput.value = '';
+
+    Swal.fire({
+        icon: "success",
+        title: "Consulta recibida.",
+    });
+};
+
+
+if (formularioContacta) {
+    formularioContacta.addEventListener('submit', enviarConsulta);
+}
